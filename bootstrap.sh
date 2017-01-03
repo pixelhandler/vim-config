00cb44c2a30efd8bd8774a50939c345e4ad02c2a
Bill Heaton <pixelhandler@gmail.com>
2017-01-02 22:54:16 -0800

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
-mkdir ~/.vim/_backup
-mkdir ~/.vim/_temp
+mkdir -p ~/.vim/_backup
+mkdir -p ~/.vim/_temp
 ln -s ~/.vim/.vimrc ~/.vimrc
 
